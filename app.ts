6814b411a0d167aedad9621b6850f872ec588484
hoangviethung071195 <hoangviethung071195@gmail.com>
2023-10-05 22:30:45 +0700

    remove json() from app.ts file

--- app.ts
+++ app.ts
@@ -1,18 +1,16 @@
 import express from 'express';
-import { getThirdParties } from './src/services/services';
-import { setAccessControl } from './src/middleware/handler/meta-data-header';
-import { handleError } from './src/middleware/handler/error';
-import { routes } from './src/routes/routes';
-import { db } from './src/db/db';
-import { PORT } from './src/util/constant/env';
 import { connectSocket } from './socket';
-import { json } from 'body-parser';
-import { readFileSync } from 'fs';
+import { db } from './src/db/db';
+import { handleError } from './src/middleware/handler/error';
+import { setAccessControl } from './src/middleware/handler/meta-data-header';
+import { routes } from './src/routes/routes';
+import { getThirdParties } from './src/services/services';
+import { PORT } from './src/util/constant/env';
 
 const app = express();
 
 app.use(getThirdParties());
-app.use(json())
+
 app.use(setAccessControl);
 
 app.use(routes);
